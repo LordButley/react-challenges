d44ce037f8c262c026c325af0208342d7427cf76
LordButley <joshjwwilson@gmail.com>
2023-03-02 14:31:36 +0000

    challenge1

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// function Sidebar(){
+//     // let SideBarContent = 
+//     // <div>
+//     //     <a></a>
+//     //     <a></a>
+//     //     <a></a>
+//     // </div>; 
+//     return <div>
+//         <a>My Photos</a>
+//         <a>My Illustrations</a>
+//         <a>My Paintings</a>
+//     </div>; 
+// }
+
+const Sidebar = () => <div>
+    <a href="#.">My Photos</a>
+    <a href="#.">My Illustrations</a>
+    <a href="#.">My Paintings</a>
+    </div>;
+
+
+export default Sidebar;
