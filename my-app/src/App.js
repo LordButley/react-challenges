3df6b87ea021c231b728ace15b69215e53dbb301
LordButley <joshjwwilson@gmail.com>
2023-03-03 14:07:13 +0000

    add code up to updating state pt2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+// import Sidebar from './components/Sidebar';
+import SidebarClass from './components/SidebarClass';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      {/* <Sidebar /> */}
+
+      {/* <SidebarClass /> */}
+      <SidebarClass anchorOne="Snaps" anchorTwo="Friends" />
+
     </div>
   );
 }
---
+++ my-app/src/components/SidebarClass.js
@@ -0,0 +1,142 @@
+import React from "react";
+import css from "./css/Sidebar.module.css"
+
+// PART 1 basic class
+
+// class SidebarClass extends React.Component{
+//     render () {
+//      return <div className = {css.sidebar}>
+//         <a href="#.">My Pictures</a>
+//         <a href="#.">My Illustrations</a>
+//         <a href="#.">My Paintings</a>
+//     </div>;
+//     }
+// }
+
+// PART 2 - With PROPS
+
+// class SidebarClass extends React.Component{
+//         render () {
+//          return <div className = {css.sidebar}>
+//             <a href="#.">{this.props.anchorOne}</a>
+//             <a href="#.">{this.props.anchorTwo}</a>
+//             <a href="#.">My Paintings</a>
+//         </div>;
+//         }
+//     }
+
+// PART 3 - With STATE
+
+// class SidebarClass extends React.Component{
+    
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             possession: "My",
+//             buttonText: "Exit",
+//         }
+//     }
+    
+//     render () {
+//      return (<div className = {css.sidebar}>
+//         <a href="#.">{this.state.possession} {this.props.anchorOne}</a>
+//         <a href="#.">{this.state.possession} {this.props.anchorTwo}</a>
+//         <a href="#.">My Paintings</a>
+//         <button>{this.state.buttonText}</button>
+//         </div>);
+//     }
+// }
+
+// PART 4 with setState / onClick 
+// class SidebarClass extends React.Component{
+    
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             possession: "My",
+//             buttonText: "Exit",
+//             // clickCounter: 0,
+//         }
+//     }
+
+    // updateClick(){
+    //     let addClick = this.state.clickCounter + 1;
+    //     if (this.state.clickCounter % 2 === 0){
+    //         this.setState({
+    //             possession:"Yours",
+    //             clickCounter: addClick,
+    //         });
+    //     }else {
+    //         this.setState({
+    //             possession:"My",
+    //             clickCounter: addClick,
+    //         });
+    //     }
+    // }
+
+//     updateClick(){
+//         if (this.state.possession == "My"){
+//             this.setState({
+//                 possession:"Yours",
+//             });
+//         }else {
+//             this.setState({
+//                 possession:"My",
+//             });
+//         }
+//     }
+    
+//     render () {
+//      return (<div className = {css.sidebar}>
+//         <a href="#.">{this.state.possession} {this.props.anchorOne}</a>
+//         <a href="#.">{this.state.possession} {this.props.anchorTwo}</a>
+//         <a href="#.">My Paintings</a>
+//         <button onClick={() => this.updateClick()}>{this.state.buttonText}</button>
+//         </div>);
+//     }
+// }
+
+
+// export default SidebarClass
+
+// PART 5 with call back logs
+
+class SidebarClass extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            possession: "My",
+            buttonText: "Exit",
+            // clickCounter: 0,
+        }
+    }
+
+    updateClick(){
+        if (this.state.possession === "My"){
+            this.setState({
+                possession:"Yours",
+            }, () => console.log("state after: " +this.state.possession));
+        }else {
+            this.setState({
+                possession:"My",
+            }, () => {
+                console.log("state after: " +this.state.possession)
+            });
+        }
+        console.log("state before: " +this.state.possession);
+    }
+
+    render () {
+        return (
+            <div className = {css.sidebar}>
+                <a href="#.">{this.state.possession} {this.props.anchorOne}</a>
+                <a href="#.">{this.state.possession} {this.props.anchorTwo}</a>
+                <a href="#.">My Paintings</a>
+                <button onClick={() => this.updateClick()}>{this.state.buttonText}</button>
+            </div>);
+        }
+}
+
+
+export default SidebarClass
